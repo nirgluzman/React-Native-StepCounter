e2ef6ce9bc2d3edf2d04585eeb74172b80cf3b92
Nir Gluzman <nir.gluzman@gmail.com>
2024-12-18 18:18:20 +0100

    add health data by calling to useHealthData

--- App.tsx
+++ App.tsx
@@ -1,18 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+
+import { AntDesign } from '@expo/vector-icons'; // arrows for the date picker
+
+import { useState } from 'react';
 
 import { Value } from './src/components/Value';
 import { RingProgress } from './src/components/RingProgress';
 
+import useHealthData from './src/hooks/useHealthData';
+
+const STEPS_GOAL = 10_000;
+
 export default function App() {
+  const [date, setDate] = useState<Date>(new Date());
+
+  // extract health data
+  const { steps, distance, floors } = useHealthData(date);
+
+  const changeDate = (numDays: number) => {
+    const currentDate = new Date(date); // create a copy of the current date.
+    currentDate.setDate(currentDate.getDate() + numDays); // update the date by adding/subtracting the number of days.
+    setDate(currentDate); // update the state variable.
+  };
+
   return (
     <View style={styles.container}>
-      <RingProgress radius={150} strokeWidth={50} progress={0.25} />
+      {/* date picker */}
+      <View style={styles.datePicker}>
+        <AntDesign onPress={() => changeDate(-1)} name='left' size={20} color='#C3FF53' />
+        <Text style={styles.date}>{date.toDateString()}</Text>
+        <AntDesign onPress={() => changeDate(1)} name='right' size={20} color='#C3FF53' />
+      </View>
+
+      {/* progress ring */}
+      <RingProgress radius={150} strokeWidth={50} progress={steps / STEPS_GOAL} />
 
       <View style={styles.values}>
-        <Value label='Steps' value='1209' />
-        <Value label='Distance' value='1,25 km' />
-        <Value label='Flights Climbed' value='12' />
+        <Value label='Steps' value={steps.toString()} />
+        <Value label='Distance' value={`${(distance / 1000).toFixed(2)} km`} />
+        <Value label='Floors climbed' value={floors.toString()} />
       </View>
 
       <StatusBar style='auto' />
@@ -34,5 +61,19 @@ const styles = StyleSheet.create({
     gap: 25, // sets the size of the gap (gutter) between rows and columns.
     flexWrap: 'wrap', // controls whether the flex items are forced onto one line or can wrap onto multiple lines.
     marginTop: 100,
+  },
+
+  datePicker: {
+    alignItems: 'center',
+    padding: 20,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+
+  date: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: '500',
+    marginHorizontal: 20,
   },
 });
